98fe6132740eb86c6709c8c5313a29a2bab41d00
ningalu <thomasholster2012@gmail.com>
2022-10-29 16:16:01 +1300

    fix clock type

--- src/nge3/nge/Timer.cpp
+++ src/nge3/nge/Timer.cpp
@@ -5,7 +5,7 @@ Timer::Timer() { start_ = std::chrono::high_resolution_clock::now(); }
 
 long double Timer::GetElapsedTime() const {
   return static_cast<std::chrono::duration<double>>(
-           std::chrono::steady_clock::now() - start_
+           std::chrono::high_resolution_clock::now() - start_
   )
     .count();
 }
