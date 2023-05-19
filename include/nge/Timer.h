6718bae052134358d8bf5b607c9c13b4ff93b0aa
ningalu <thomasholster2012@gmail.com>
2023-05-19 12:03:20 +1200

    allow timer offsets

--- include/nge/Timer.h
+++ include/nge/Timer.h
@@ -10,13 +10,15 @@ public:
   Timer(double timescale);
 
   void Restart() noexcept;
-  [[nodiscard]] long double GetElapsedTime() const noexcept;
+  void Restart(double offset) noexcept;
+  [[nodiscard]] double GetElapsedTime() const noexcept;
   [[nodiscard]] double GetTimescale() const noexcept;
   void SetTimescale(double timescale) noexcept;
 
 protected:
   std::chrono::time_point<std::chrono::high_resolution_clock> start_;
   double timescale_;
+  double offset_;
 };
 } // namespace nge
 
--- src/nge/Timer.cpp
+++ src/nge/Timer.cpp
@@ -1,23 +1,27 @@
 #include "nge/Timer.h"
 
 namespace nge {
-Timer::Timer() : timescale_(1.0) {
+Timer::Timer() : timescale_(1.0), offset_(0.0) {
   start_ = std::chrono::high_resolution_clock::now();
 }
-Timer::Timer(double timescale) : timescale_(timescale) {
+Timer::Timer(double timescale) : timescale_(timescale), offset_(0.0) {
   start_ = std::chrono::high_resolution_clock::now();
 }
-long double Timer::GetElapsedTime() const noexcept {
+double Timer::GetElapsedTime() const noexcept {
   return static_cast<std::chrono::duration<double>>(
            std::chrono::high_resolution_clock::now() - start_
-  )
-    .count();
+         )
+           .count()
+         - offset_;
 }
 
 void Timer::Restart() noexcept {
   start_ = std::chrono::high_resolution_clock::now();
 }
-
+void Timer::Restart(double offset) noexcept {
+  start_ = std::chrono::high_resolution_clock::now();
+  offset_ = offset;
+}
 [[nodiscard]] double Timer::GetTimescale() const noexcept { return timescale_; }
 void Timer::SetTimescale(double timescale) noexcept { timescale_ = timescale; }
 } // namespace nge
