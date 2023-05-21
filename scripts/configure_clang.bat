20f3a7891849a826f1923217c89c5bdd460217c4
ningalu <thomasholster2012@gmail.com>
2023-05-21 21:38:28 +1200

    try other compilers

---
+++ scripts/configure_clang.bat
@@ -0,0 +1,16 @@
+@REM same msys2 issue
+
+mkdir build
+cd ./build
+mkdir Debug
+mkdir Release
+
+cd Debug
+conan install ../.. --output-folder=. --build=missing -pr:b=clang -pr:h=clang --update -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True -s build_type=Debug
+cd ../..
+cmake -S . -B ./build/Debug -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE="./build/Debug/conan_toolchain.cmake"
+
+cd ./build/Release
+conan install ../.. --output-folder=. --build=missing -pr:b=clang -pr:h=clang --update -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True -s build_type=Release
+cd ../..
+cmake -S . -B ./build/Release -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE="./build/Release/conan_toolchain.cmake"
---
+++ scripts/configure_mingw.bat
@@ -0,0 +1,17 @@
+@REM fails; appears to be an MSYS2 issue that isnt fixed in conancenter yet
+
+
+mkdir build
+cd ./build
+mkdir Debug
+mkdir Release
+
+cd Debug
+conan install ../.. --output-folder=. --build=missing -pr:b=mingw -pr:h=mingw --update -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True -s build_type=Debug
+cd ../..
+cmake -S . -B ./build/Debug -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE="./build/Debug/conan_toolchain.cmake"
+
+cd ./build/Release
+conan install ../.. --output-folder=. --build=missing -pr:b=mingw -pr:h=mingw --update -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True -s build_type=Release
+cd ../..
+cmake -S . -B ./build/Release -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE="./build/Release/conan_toolchain.cmake"
