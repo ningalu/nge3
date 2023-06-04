38c338be605514ea83a9e3fa127b23b705288ead
ningalu <thomasholster2012@gmail.com>
2023-06-05 02:41:23 +1200

    build in wsl

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,17 @@
-#!/bin/zsh
+#!/bin/sh
 
-# why is this literally the easiest solution
-# alias gcc="gcc-10"
-# alias g++="g++-10"
-# export CC=gcc-10
-# export CXX=g++-10
+# std::bit_cast only starts being supported in gcc>11.1
+# its literally not on the gcc website
+# why is finding out how to change the default version of a program in linux so fucking obscure
+# sudo apt install gcc-11
+# sudo update-alternatives --remove-all gcc 
+# sudo update-alternatives --remove-all g++
+# sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 10
+# sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-11 10
+# sudo update-alternatives --install /usr/bin/cc cc /usr/bin/gcc 30
+# sudo update-alternatives --set cc /usr/bin/gcc
+# sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++ 30
+# sudo update-alternatives --set c++ /usr/bin/g++
 
 cmake --build ./build/Debug --config Debug
 cmake --build ./build/Release --config Release
