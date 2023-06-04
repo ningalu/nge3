9f399faa31749b1fadb4ffe1544be2a687c713f1
ningalu <thomasholster2012@gmail.com>
2023-06-04 19:05:06 +1200

    tests+assertions on size and layout

---
+++ tests/misc/PointTests.cpp
@@ -0,0 +1,38 @@
+#include "gtest/gtest.h"
+
+#include <type_traits>
+
+#include "misc/Point.hpp"
+
+TEST(PointTests, Access) {
+  ngl::Point<int32_t> p{20, 30};
+  ASSERT_EQ(p.X(), 20);
+  static_assert(std::is_same<decltype(p.X()), int32_t>::value);
+  ASSERT_EQ(p.Y(), 30);
+  static_assert(std::is_same<decltype(p.Y()), int32_t>::value);
+  ASSERT_EQ(p.X<double>() + 0.5, 20.5);
+  static_assert(std::is_same<decltype(p.X<double>()), double>::value);
+  ASSERT_EQ(p.Y<double>() + 0.5, 30.5);
+  static_assert(std::is_same<decltype(p.Y<double>()), double>::value);
+}
+
+TEST(PointTests, Within_Enclosed_NoEnclosedAxis) {
+  ngl::Point<int32_t> p{0, 0};
+  ngl::Rectangle<int32_t> r{10, 10, 100, 100};
+  EXPECT_FALSE(p.Within(r));
+}
+TEST(PointTests, Within_Enclosed_XAxis) {
+  ngl::Point<int32_t> p{200, 30};
+  ngl::Rectangle<int32_t> r{10, 10, 100, 100};
+  EXPECT_FALSE(p.Within(r));
+}
+TEST(PointTests, Within_Enclosed_YAxis) {
+  ngl::Point<int32_t> p{20, 200};
+  ngl::Rectangle<int32_t> r{10, 10, 100, 100};
+  EXPECT_FALSE(p.Within(r));
+}
+TEST(PointTests, Within_Enclosed_FullyEnclosed) {
+  ngl::Point<int32_t> p{20, 30};
+  ngl::Rectangle<int32_t> r{10, 10, 100, 100};
+  EXPECT_TRUE(p.Within(r));
+}
